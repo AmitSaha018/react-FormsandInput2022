098422e2ffab8c0532182a5a8560280306fba115
AmitSaha018 <31251262+AmitSaha018@users.noreply.github.com>
2022-02-18 18:02:00 +0530

    taking input with useRef.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
 
   const nameInputChangeHandler = (event) => {
@@ -11,12 +12,18 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     console.log(enteredName)
+    
+    const enteredValue = nameInputRef.current.value
+    // nameInputRef.current.value = '' => XXXXXXXXXXXXXXXX Don't manipulate the DOm directly.
+    console.log(enteredValue)
+    setEnteredName('')
   };
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
         <input
+          ref={nameInputRef}
           type="text"
           id="name"
           value={enteredName}
